4ce08dbfcce3a1f5d30f7d6bc8458277b3eb801e
Luis Viruena <luis.viruena@gmail.com>
2024-06-15 17:46:06 -0400

    Fix welcome screen

--- app/src/main/java/com/outerspace/flow_experiment/ui/main/MainActivity.kt
+++ app/src/main/java/com/outerspace/flow_experiment/ui/main/MainActivity.kt
@@ -5,11 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
+import com.outerspace.flow_experiment.R
+import com.outerspace.flow_experiment.data.PictureItem
 import com.outerspace.flow_experiment.ui.theme.FlowExperimentTheme
 import kotlinx.coroutines.launch
 
@@ -27,8 +42,46 @@ class MainActivity : ComponentActivity() {
                     modifier = modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    welcome(picturesVM.picturesState, modifier)
+                    Column(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        // Weight is essential here so the scrollable content is filling all the available space
+                        scrollableContent(pictureList = picturesVM.picturesState, modifier = Modifier.weight(1f))
+                        buttonsContainer(onClick = onClickRefresh)
+                    }
+
+//                    Column(modifier = modifier.fillMaxSize()) {
+//                        welcome(picturesVM.picturesState, modifier = modifier.weight(0.8f))
+//                    }
+//                    Button(onClick = onClickRefresh, modifier = Modifier.fillMaxWidth()) {
+//                        Text(stringResource(R.string.refresh_button_face))
+//                    }
                 }
+            }
+        }
+    }
+
+    @Composable
+    private fun scrollableContent(pictureList: SnapshotStateList<PictureItem>, modifier: Modifier) {
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState()),
+        ) {
+            welcome(modifier, pictureList)
+        }
+    }
+
+    @Composable
+    private fun buttonsContainer(onClick: () -> Unit) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 50.dp),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Button(onClick = onClickRefresh, modifier = Modifier.fillMaxWidth()) {
+                Text(stringResource(R.string.refresh_button_face))
             }
         }
     }
@@ -43,6 +96,12 @@ class MainActivity : ComponentActivity() {
 
             // collect pictures collects from the flow, which in turn emits the result from the service
             picturesVM.collectPictures("Ganesh", 1, 5)
+        }
+    }
+
+    private val onClickRefresh: () -> Unit = {
+        lifecycleScope.launch {
+            picturesVM.collectPictures("Ganesh", 2, 5)
         }
     }
 }
--- app/src/main/java/com/outerspace/flow_experiment/ui/main/WelcomeScreen.kt
+++ app/src/main/java/com/outerspace/flow_experiment/ui/main/WelcomeScreen.kt
@@ -1,20 +1,28 @@
 package com.outerspace.flow_experiment.ui.main
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.outerspace.flow_experiment.R
 import com.outerspace.flow_experiment.data.PictureItem
 import com.outerspace.flow_experiment.ui.theme.FlowExperimentTheme
 
 @SuppressLint("ComposableNaming")
 @Composable
-fun welcome(pictureList: SnapshotStateList<PictureItem>, modifier: Modifier = Modifier) {
+fun welcome(modifier: Modifier = Modifier, pictureList: SnapshotStateList<PictureItem>) {
     Column(modifier = modifier) {
         pictureList.forEach { pictureItem ->
             Text (
@@ -35,6 +43,6 @@ fun welcomePreview() {
         )
     }
     FlowExperimentTheme {
-        welcome(pictureList, Modifier)
+        welcome(Modifier, pictureList)
     }
 }
